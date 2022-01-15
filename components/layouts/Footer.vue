dfea06bc95d7b4eed6af872e9940f8a67c53eb59
ryota050505 <05ryouta05@gmail.com>
2022-01-16 02:22:09 +0900

    #1 Create Footer Component

---
+++ components/layouts/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-footer
+    app
+    :absolute=true
+  >
+    <span>
+      &copy; {{ new Date().getFullYear() }}
+      りょーたの遊び場
+    </span>
+  </v-footer>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'LayoutsFooter'
+})
+</script>
