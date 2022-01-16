f11e2dbd5f869d4383af1f2f3b1d0d7a656cc89c
ryota050505 <05ryouta05@gmail.com>
2022-01-16 18:25:16 +0900

    #4 Add container

--- pages/news.vue
+++ pages/news.vue
@@ -2,12 +2,14 @@
   <v-row>
     <v-col>
       <v-sheet min-height="70vh" rounded="lg">
-        <title>{{ posts.city.name }}</title>
-        {{ date.getMonth()+1 }}月{{ date.getDate() }}日{{WEEK[date.getDay()] + date.getHours() + " :00"}}
-        <div>
-          {{ posts.list[0].weather[0].icon }}<br>
-          {{ Math.round(posts.list[0].main.temp) }}<br>
-        </div>
+        <v-containrer>
+          <title>{{ posts.city.name }}</title>
+          {{ date.getMonth()+1 }}月{{ date.getDate() }}日{{WEEK[date.getDay()] + date.getHours() + " :00"}}
+          <div>
+            {{ posts.list[0].weather[0].icon }}<br>
+            {{ Math.round(posts.list[0].main.temp) }}<br>
+          </div>
+        </v-containrer>
       </v-sheet>
     </v-col>
   </v-row>
