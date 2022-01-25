117cfcbd0b3da5d5c73fb1b797333be8e0ee52c2
ryota050505 <05ryouta05@gmail.com>
2022-01-25 09:07:28 +0900

    #10 icon setting

--- components/materials/Logo.vue
+++ components/materials/Logo.vue
@@ -1,5 +1,9 @@
 <template>
-  <v-avatar class="mr-10" color="grey darken-1" size="32"></v-avatar>
+  <v-avatar class="mr-10" color="grey darken-1" size="32">
+    <v-img
+      :src="require('@/assets/img/takoyaki.png')"
+    />
+  </v-avatar>
 </template>
 
 <script lang="ts">
