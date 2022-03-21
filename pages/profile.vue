7933bafda5d02488f8a1cedeb14bfa328b6e3eba
ryota050505 <05ryouta05@gmail.com>
2022-03-22 04:59:59 +0900

    :+1: プロフィールにタイムツリー追加

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,19 +1,36 @@
 <template>
   <v-row>
-    <v-col cols="2">
+    <v-col
+      cols="12"
+      xl="2"
+      lg="2"
+      md="2"
+      sm="12"
+      xs="12"
+    >
       <v-sheet rounded="lg">
         <v-list color="transparent">
-          <v-list-item v-for="n in 5" :key="n" link>
-            <v-list-item-content>
-              <v-list-item-title> List Item {{ n }} </v-list-item-title>
+          <v-list-item
+            v-for="(type, i) in types" :key="i"
+            link
+          >
+            <v-list-item-content
+              @click="filterTimeline(type.type)"
+            >
+              <v-list-item-title>{{ type.title }} </v-list-item-title>
             </v-list-item-content>
           </v-list-item>
 
           <v-divider class="my-2"></v-divider>
 
-          <v-list-item link color="grey lighten-4">
+          <v-list-item color="grey lighten-4">
             <v-list-item-content>
-              <v-list-item-title> Refresh </v-list-item-title>
+              <v-switch
+                v-model="denseTrigger"
+                label="dense"
+                :disabled=$vuetify.breakpoint.xs
+              >
+              </v-switch>
             </v-list-item-content>
           </v-list-item>
         </v-list>
@@ -21,16 +38,118 @@
     </v-col>
 
     <v-col>
-      <v-sheet min-height="70vh" rounded="lg">
-        <!--  -->
-      </v-sheet>
+      <v-timeline
+        light
+        :align-top=denseTrigger
+        :dense=denseTrigger
+      >
+        <v-timeline-item
+          v-for="(item, i) in Timeline"
+          :key="i"
+          :color="item.color"
+          :small=$vuetify.breakpoint.smAndDown
+        >
+          <v-row
+            v-if="denseTrigger"
+            class="pt-1 text-center"
+          >
+            <v-col cols="3">
+              <span
+                :class="`font-weight-bold
+                  ${item.color}--text
+                  ${$vuetify.breakpoint.xs ? 'text-caption': ''}
+                `"
+                v-text="item.year"
+              ></span>
+            </v-col>
+            <v-col>
+              <v-card
+                :color="item.color"
+                dark
+              >
+                <v-card-title
+                  :class="`
+                    headline
+                    font-weight-light
+                    ${$vuetify.breakpoint.xs ? 'text-subtitle-1': ''}
+                  `"
+                >
+                  {{ item.title }}
+                </v-card-title>
+                <v-card-text
+                  v-if="item.body"
+                  :class="`
+                    white
+                    black--text
+                    text-left
+                    ${$vuetify.breakpoint.xs ? 'text-body-2': ''}
+                  `"
+                >
+                  {{ item.body }}
+                </v-card-text>
+              </v-card>
+            </v-col>
+          </v-row>
+          <template
+            v-if="!denseTrigger"
+            #opposite
+          >
+            <span
+              :class="`
+                font-weight-bold
+                ${item.color}--text text-center
+                ${$vuetify.breakpoint.xs ? 'text-caption': ''}
+              `"
+              v-text="item.year"
+            >
+            </span>
+          </template>
+          <v-card
+            v-if="!denseTrigger"
+            :color="item.color"
+            dark
+          >
+            <v-card-title
+              :class="`
+                headline
+                font-weight-light
+                ${$vuetify.breakpoint.xs ? 'text-subtitle-1': ''}
+              `"
+            >
+              {{ item.title }}
+            </v-card-title>
+            <v-card-text
+              v-if="item.body"
+              :class="`
+                white
+                black--text
+                text-left
+                ${$vuetify.breakpoint.xs ? 'text-body-1': ''}
+              `"
+            >
+              {{ item.body }}
+            </v-card-text>
+          </v-card>
+        </v-timeline-item>
+      </v-timeline>
     </v-col>
+    <v-col
+      v-if=!$vuetify.breakpoint.smAndDown
+      cols="2"
+    />
   </v-row>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'nuxt-property-decorator'
+import {
+  Component,
+  Vue
+} from 'nuxt-property-decorator'
 
+import {
+  TIMELINE,
+  TIMELINE_KEY,
+} from '@/plugins/constants'
 import { LocalHeader } from '~/types/LocalHeader'
 @Component
 export default class ProfilePage extends Vue {
@@ -39,5 +158,40 @@ export default class ProfilePage extends Vue {
       title: 'Profile',
     }
   }
+
+  private denseTrigger = this.$vuetify.breakpoint.smAndDown
+
+  private timeline = TIMELINE
+
+  get Timeline() {
+    return this.timeline
+  }
+
+  set Timeline(timeline) {
+    this.timeline = timeline
+  }
+
+  private filterTimeline(type :string) :void {
+    this.Timeline = type === TIMELINE_KEY.ALL ? TIMELINE : TIMELINE.filter(x => x.key === type)
+  }
+
+  types = [
+    {
+      type: TIMELINE_KEY.ALL,
+      title: '全て',
+    },
+    {
+      type: TIMELINE_KEY.SCHOOL,
+      title: '学校',
+    },
+    {
+      type: TIMELINE_KEY.PARTTIME,
+      title: 'アルバイト'
+    },
+    {
+      type: TIMELINE_KEY.INTERNSHIP,
+      title: 'インターン',
+    }
+  ]
 }
 </script>
--- plugins/constants.ts
+++ plugins/constants.ts
@@ -78,6 +78,86 @@ export const FOOTER_CONTACT_LIST = [
   // }
 ]
 
+export enum TIMELINE_KEY {
+  SCHOOL = 'SCHOOL',
+  PARTTIME = 'PARTTIME',
+  INTERNSHIP = 'INTERNSHIP',
+  ALL = 'ALL',
+}
+
+export const TIMELINE = [
+  {
+    color: 'purple',
+    year: '2017/4',
+    title: '同志社大学　入学',
+    body: '情報学について学び始めます',
+    key: TIMELINE_KEY.SCHOOL,
+  },
+  {
+    color: 'green',
+    year: '2020/12',
+    title: '株式会社Donuts　アルバイト入社',
+    body: 'ソーシャルゲーム「単車の虎」開発に従事',
+    key: TIMELINE_KEY.PARTTIME,
+  },
+  {
+    color: 'purple',
+    year: '2021/3',
+    title: '同志社大学　卒業',
+    body: '',
+    key: TIMELINE_KEY.SCHOOL,
+  },
+  {
+    color: 'purple',
+    year: '2021/4',
+    title: '同志社大学大学院　入学',
+    body: '音声認識について研究しています',
+    key: TIMELINE_KEY.SCHOOL,
+  },
+  {
+    color: 'green',
+    year: '2021/5',
+    title: '某企業　アルバイト入社',
+    body: '',
+    key: TIMELINE_KEY.PARTTIME,
+  },
+  {
+    color: 'blue',
+    year: '2021/8~9',
+    title: 'ヤフー株式会社　インターン',
+    body: '2週間程、フルタイムで実務を経験させていただきました',
+    key: TIMELINE_KEY.INTERNSHIP,
+  },
+  {
+    color: 'blue',
+    year: '2021/9',
+    title: 'Sansan株式会社　インターン',
+    body: '課題解決型の1dayインターンでした',
+    key: TIMELINE_KEY.INTERNSHIP
+  },
+  {
+    color: 'blue',
+    year: '2021/9',
+    title: '楽天株式会社　インターン',
+    body: '2週間程度のハッカソンでした',
+    key: TIMELINE_KEY.INTERNSHIP,
+  },
+  {
+    color: 'green',
+    year: '2021/9',
+    title: '株式会社Donuts　退社',
+    body: '',
+    key: TIMELINE_KEY.PARTTIME,
+  },
+  {
+    color: 'purple',
+    year: '2023/3',
+    title: '同志社大学大学院　修了予定',
+    body: '',
+    key: TIMELINE_KEY.SCHOOL,
+  },
+]
+
 export default (_: any, inject: any) => {
   inject('MAX_WIDTH_SP', MAX_WIDTH_SP)
   inject('ENTER_KEY_CODE', ENTER_KEY_CODE)
