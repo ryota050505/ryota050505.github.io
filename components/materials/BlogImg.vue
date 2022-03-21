547f4d47887c39ae586e13b0149da352d80c4575
ryota050505 <05ryouta05@gmail.com>
2022-03-21 21:21:32 +0900

    :bug: Add Google G-TAG

---
+++ components/materials/BlogImg.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-img
+    :src="require(`@/assets/img/${imgSrc}`)"
+    class="my-5 mx-5"
+    contain
+    style="position: relative;"
+    max-height="50vh"
+  >
+    <slot/>
+  </v-img>
+</template>
+
+<script lang="ts">
+import {
+  Component,
+  Prop,
+  Vue,
+} from 'nuxt-property-decorator'
+@Component
+export default class BlogImg extends Vue {
+  @Prop({ type: String, required: true })
+  private imgSrc!: string
+}
+</script>
