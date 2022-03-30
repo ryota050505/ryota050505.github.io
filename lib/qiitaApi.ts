a9443cbe697046e291a2504700fb7a5f162db059
ryota050505 <05ryouta05@gmail.com>
2022-03-30 14:53:06 +0900

    ログ出力

--- lib/qiitaApi.ts
+++ lib/qiitaApi.ts
@@ -8,6 +8,9 @@ export const getQiitaPosts = async () => {
   const nowdate = new Date()
   const limitdate = formatISO(nowdate.setDate(nowdate.getDate() - 10), { representation: 'date' })
 
+  console.log("QIITA_URL", QIITA_URL)
+  console.log("QIITA_API_KEY", QIITA_API_KEY)
+
   try{
     const res = await axios.get(`${QIITA_URL}items?page=1&per_page=20&query=created%3A%3E${limitdate}+stocks%3A%3E20`,
       {
