ee2390da0b9faf4311e6b0a9562182cc0270b0cb
ryota-sub <sp.ryota.05@gmail.com>
2022-04-02 01:31:38 +0900

    :+1: リダイレクト

--- middleware/trailingSlashRedirect.ts
+++ middleware/trailingSlashRedirect.ts
@@ -2,7 +2,8 @@ import type { Middleware } from "@nuxt/types"
 
 const trailingSlashRedirect: Middleware = ({ route, redirect }) => {
   if (route.path.slice(-1) === '/') {
-    redirect(301, route.path.substring(0, route.path.length - 1))
+    console.log("リダイレクトする予定")
+    // redirect(301, route.path.substring(0, route.path.length - 1))
   }
 }
 
