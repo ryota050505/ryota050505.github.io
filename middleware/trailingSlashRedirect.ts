9616858687694098478788e9146307c588e5d5d8
ryota-sub <sp.ryota.05@gmail.com>
2022-04-02 01:43:23 +0900

    :bug: 末尾スラッシュ有りに統一

--- middleware/trailingSlashRedirect.ts
+++ middleware/trailingSlashRedirect.ts
@@ -1,9 +1,9 @@
 import type { Middleware } from "@nuxt/types"
 
 const trailingSlashRedirect: Middleware = ({ route, redirect }) => {
-  if (route.path.slice(-1) === '/') {
+  if (route.path.slice(-1) !== '/') {
     console.log("リダイレクトする予定")
-    // redirect(301, route.path.substring(0, route.path.length - 1))
+    redirect(301, route.path + '/')
   }
 }
 
