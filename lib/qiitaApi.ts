0286e98ebedb930507d02573c440798fd087c7ee
ryota-sub <sp.ryota.05@gmail.com>
2022-04-01 23:15:57 +0900

    :+1: publicRuntimeConfigを用いた環境変数の利用

--- lib/qiitaApi.ts
+++ lib/qiitaApi.ts
@@ -1,18 +1,16 @@
 import axios from 'axios'
 import { formatISO } from 'date-fns'
 
-const QIITA_URL = process.env.QIITA_ENDPOINT_URL
-const QIITA_API_KEY = process.env.QIITA_API_KEY
+export const getQiitaPosts = async (QIITA_ENDPOINT_URL: string, QIITA_API_KEY: string) => {
 
-export const getQiitaPosts = async () => {
   const nowdate = new Date()
   const limitdate = formatISO(nowdate.setDate(nowdate.getDate() - 10), { representation: 'date' })
 
-  console.log("QIITA_URL", QIITA_URL)
+  console.log("QIITA_URL", QIITA_ENDPOINT_URL)
   console.log("QIITA_API_KEY", QIITA_API_KEY)
 
   try{
-    const res = await axios.get(`${QIITA_URL}items?page=1&per_page=20&query=created%3A%3E${limitdate}+stocks%3A%3E20`,
+    const res = await axios.get(`${QIITA_ENDPOINT_URL}items?page=1&per_page=20&query=created%3A%3E${limitdate}+stocks%3A%3E20`,
       {
         headers:
         {
